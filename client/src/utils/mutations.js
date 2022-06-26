94e9a624ddca5ba35041a57e7860b3e752d0da0b
sergii-onyx <twilight.cympak@gmail.com>
2022-06-26 17:50:45 -0400

    added auth, mutations and queries

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,99 @@
+import { gql } from "@apollo/client";
+
+export const ADD_USER = gql`
+  mutation register(
+    $username: String!
+    $email: String!
+    $position: String!
+    $password: String!
+    $confirmPassword: String!
+  ) {
+    addUser(
+      username: $username
+      email: $email
+      position: $position
+      password: $password
+      confirmPassword: $confirmPassword
+    ) {
+      token
+      # CHECK if need more new user data!
+      user {
+        _id
+        username
+        # email
+        # password
+        # position
+        # projects {
+        #   _id
+        #   jobName
+        #   description
+        #   driver: [User]
+        #   startTime
+        #   pickUpAddress
+        #   deliveryAddress
+        #   createdAt
+        #   client: Client
+        # }
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      # CHECK if need more user data!
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_CLIENT = gql`
+  mutation addClient($name: String!) {
+    addClient(name: $name) {
+      _id
+      name
+    }
+  }
+`;
+
+export const ADD_PROJECT = gql`
+  mutation addProject(driverEmail: String!, clientName: String!, jobName: String!, description: String!, pickUpAddress: String!, deliveryAddress: String!) {
+    addProject(driverEmail: $driverEmail, clientName: $clientName, jobName: $jobName, description: $description, pickUpAddress: $pickUpAddress, deliveryAddress: $deliveryAddress) {
+      _id
+      jobName
+      description
+      driver{
+        _id
+        username
+        # email
+        # password
+        # position
+        # projects {
+        #   _id
+        #   jobName
+        #   description
+        #   driver: [User]
+        #   startTime
+        #   pickUpAddress
+        #   deliveryAddress
+        #   createdAt
+        #   client: Client
+        # }
+      }
+      startTime
+      pickUpAddress
+      deliveryAddress
+      createdAt
+      client{
+        _id
+        name
+        # projects
+      }
+    }
+  }
+`;
