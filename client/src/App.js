92f649df885a62cedebe856a1b9ed5046be9aef1
willduncan <willdunc12@gmail.com>
2022-06-27 10:41:55 -0400

    add header and footer basic

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,12 @@ import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Login from "./pages/Login";
-import NoMatch from "./pages/NoMatch";
-import SingleThought from "./pages/SingleThought";
-import Profile from "./pages/Profile";
-import Signup from "./pages/Signup";
-import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import NoMatch from "./pages/NoMatch";
+// import SingleThought from "./pages/SingleThought";
+// import Profile from "./pages/Profile";
+// import Signup from "./pages/Signup";
+// import Home from "./pages/Home";
 
 const httpLink = createHttpLink({
   uri: "/graphql",
@@ -42,7 +42,7 @@ function App() {
         <div className="flex-column justify-flex-start min-100-vh">
           <Header />
           <div className="container">
-            <Routes>
+            {/* <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
@@ -52,7 +52,7 @@ function App() {
               </Route>
               <Route path="/thought/:id" element={<SingleThought />} />
               <Route path="*" element={<NoMatch />} />
-            </Routes>
+            </Routes> */}
           </div>
           <Footer />
         </div>
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">&copy;2020 by Will Duncan</div>
+    </footer>
+  );
+};
+
+export default Footer;
