6c0324900d72b627d7aa71d0518cfca912b9a1b4
ApolloSolo <matt1teixeira@gmail.com>
2022-07-05 19:58:25 -0400

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -18,6 +18,16 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+ app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+ });
+
+
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   // integrate our Apollo server with the Express application as middleware
