27634aaffcedf42b1ea44712b3d27e391cbc336c
willduncan <willdunc12@gmail.com>
2022-07-05 18:28:31 -0400

    fix dashboard issues

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -6,18 +6,16 @@ import { QUERY_ME } from "../utils/queries";
 import { useMutation } from "@apollo/client";
 import { EDIT_COMPLETE, EDIT_CONFIRM } from "../utils/mutations";
 import Auth from "../utils/auth";
-import DriverMap from "../components/DriverMap"
+import DriverMap from "../components/DriverMap";
 // import JobForm from "../components/ThoughtForm";
 
 const Dashboard = () => {
-
   const [editConfirm] = useMutation(EDIT_CONFIRM, {
     errorPolicy: "all",
   });
   const [editComplete] = useMutation(EDIT_COMPLETE, {
     errorPolicy: "all",
   });
-
   const loggedIn = Auth.loggedIn();
 
   //check token to see if user dispatcher or driver
@@ -30,8 +28,9 @@ const Dashboard = () => {
   let req = QUERY_PROJECTS;
   // if user is driver req will be changed to QUERY_ME
 
-  // !!!!!!!!!! change displayBtn from "" to "none"
-  let displayBtn = "";
+  // by default Not show button for change job status
+  let displayBtn = "none";
+  // if user is driver show button and change query name
   if (userdata() === "driver") {
     req = QUERY_ME;
     displayBtn = "";
@@ -80,33 +79,35 @@ const Dashboard = () => {
 
   return (
     <main>
-      <div className="flex-row justify-space-between">
-        <div className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}>
-          <table>
+      {/* <DriverMap /> */}
+      {/* <div className="flex-row justify-space-between"> */}
+      <div
+        id="table-container"
+        // className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}
+      >
+        <table id="job-list">
+          <thead>
+            <tr>
+              <th>Status</th>
+              <th>Client name</th>
+              <th>Project name</th>
+              <th>Start Time</th>
+              <th>Driver</th>
+              <th>Pickup address</th>
+              <th>Delivery address</th>
+              <th>Description</th>
+            </tr>
+          </thead>
           {loading ? (
-            <thead>
+            <tbody>
               <tr>
-                <th>Status</th>
-                <th>Client name</th>
-                <th>Project name</th>
-                <th>Start Time</th>
-                <th>Driver</th>
-                <th>Pickup address</th>
-                <th>Delivery address</th>
-                <th>Description</th>
+                <td>Loading...</td>
               </tr>
-            </thead>
-            {loading ? (
-              <tbody>
-                <tr>
-                  <td>Loading...</td>
-                </tr>
-              </tbody>
-            ) : (
-              // if user is driver - render project
-              <>
-                {userdata() === "driver" ? (
-
+            </tbody>
+          ) : (
+            // if user is driver - render project
+            <>
+              {userdata() === "driver" ? (
                 <>
                   <JobList
                     projects={project.filter(
@@ -126,6 +127,7 @@ const Dashboard = () => {
             </>
           )}
         </table>
+
         <button
           id="confirm-complete-btn"
           style={{ display: displayBtn }}
@@ -133,9 +135,9 @@ const Dashboard = () => {
         >
           CHANGE JOB STATUS
         </button>
-        </div>
-        <DriverMap project={project}/>
+        <DriverMap project={project} />
       </div>
+      {/* </div> */}
     </main>
   );
 };
